5a5e87c52d9e03c373e77d40c0bdeb8ecace3e56
Bondsam1 <34954783+Bondsam1@users.noreply.github.com>
2019-07-11 18:11:19 +0800

    Update server.cpp

--- server.cpp
+++ server.cpp
@@ -12,7 +12,7 @@ int main(){
     struct sockaddr_in serv_addr;
     memset(&serv_addr, 0, sizeof(serv_addr));  //每个字节都用0填充
     serv_addr.sin_family = AF_INET;  //使用IPv4地址
-    serv_addr.sin_addr.s_addr = inet_addr("172.17.0.7");  //具体的IP地址
+    serv_addr.sin_addr.s_addr = inet_addr("xxx.xxx.xxx.xxx");  //具体的IP地址
     serv_addr.sin_port = htons(39001);  //端口
     bind(serv_sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
     //进入监听状态，等待用户发起请求
